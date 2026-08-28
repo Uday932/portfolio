8056c383b56ab317ade319d33437a9f7f27e2414
Uday932 <uday.bhavsar.sio@gmail.com>
2026-08-28 23:11:53 +0200

    update packages

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false, // Passe-le à false temporairement
+  turbopack: {
+    root: process.cwd(),
+  },
 };
 
 export default nextConfig;
